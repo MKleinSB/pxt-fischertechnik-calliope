8baa75772fb2588b840dcb39fae46505fb00c60e
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-04-21 18:54:56 +0200

    0.0.3

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 // tests go here; this will not be compiled when this package is used as a library
 fischertechnik.OnPinPressed(ftPins.C16, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    basic.showIcon(IconNames.Heart)
 })
 basic.forever(function () {
     if (fischertechnik.PinIsPressed(ftPins.C17)) {
